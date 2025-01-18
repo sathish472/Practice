5f1d1d1769c3cd221a2154f8d6e0b819d7990f34
Satish Mallavaram <sathish.m99@gmial.com>
2025-01-18 11:20:09 +0530

    Java Arithmetic operators

---
+++ JavaPractice/src/main/java/org/example/ArithmeticOperators.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class ArithmeticOperators {
+    public static void main (String[] args)
+    {
+        int num1 = 200;
+        int num2 = 100;
+        int sum = num1 + num2;
+        System.out.println("The sum of two numbers is: "+sum);
+        int diff = num1-num2;
+        System.out.println("The difference of two numbers is: "+diff);
+        int mul = num1*num2;
+        System.out.println("The multiplication of two numbers is: "+ mul);
+        int div = num1/num2;
+        System.out.println("The division of two numbers is: "+ div);
+        float modulus = num1%num2;
+        System.out.println("The modulus of two numbers is : "+modulus);
+    }
+}
